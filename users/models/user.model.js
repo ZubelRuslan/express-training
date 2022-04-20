51bfbd46775d34573f914ed06607fc609aa93cdd
Ruslan Zubel <ruslan.zubel.ext@sennder.com>
2022-04-20 12:47:49 +0300

    User password hashing

--- users/models/user.model.js
+++ users/models/user.model.js
@@ -1,8 +1,35 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-export const UserModel = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   name: String,
   surname: String,
   email: String,
-  password: String
-});
+  password_hash: String
+})
+
+UserSchema.virtual('password')
+  .set(function(password) {
+    this._password = password;
+    this.password_hash = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+UserSchema.methods = {
+  authenticate: function(password) {
+    return bcrypt.compareSync(password, this.password_hash);
+  },
+
+  encryptPassword: function(password) {
+    if (!password) return '';
+    try {
+      return bcrypt.hashSync(password, 10);
+    } catch (err) {
+      return '';
+    }
+  },
+};
+
+export const UserModel = mongoose.model('User', UserSchema);
